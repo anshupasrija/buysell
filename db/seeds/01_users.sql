143675cd7f4c80700159038732adc20190bbccd8
Mrdo Mrdo <mayilyane9@gmail.com>
2021-09-20 10:28:18 -0700

    Commit or modify all changes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, password, email, location, phone) VALUES ('Alice', 'labber', 'alice@gmail.com', 'Vancouver', 456789023);
+INSERT INTO users (name, password, email, location, phone) VALUES ('Kira', 'labber', 'kira@gmail.com', 'Vancouver', 234687650);
