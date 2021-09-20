ea2b426db4777c92f576d95950914494510a9a06
anshu pasrija <anshu.pasrija@gmail.com>
2021-09-20 23:40:51 +0000

    data into seeds

---
+++ public/scripts/user.js
@@ -0,0 +1,25 @@
+
+// $(document).ready(function(){
+//   const creatUserElement =$(`
+//    <h1 class="vehicles-on-sale">VEHICLES ON SALE</h1>
+//     <section class="shopping">
+//       <div class="shopping_subcontainer">
+//         <h4 class="shopping_heading">Audi S7</h4>
+//         <img src="../images/audiseven.png" class="shopping_image" alt="vehicle" class="vehicleimg"/>
+//       </div>
+//         <div class="information">
+//           <p>Year: 2019</p>
+//           <p>Price: 75,000$</p>
+//           <p>Status: ON SALE!</p>
+//           <p>Color: White</p>
+//           <p>Mileage: 14,000 km</p>
+//           <p>Transmission: automatic</p>
+//           <p>Fuel Type: Regular</p>
+//           <button class="send-a-message">SEND A MESSAGE</button>
+//         </div>
+//     </section>`
+
+//   `)
+// }
+
+
