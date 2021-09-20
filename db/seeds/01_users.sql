7414fbd2ff60e0160add4fe27e0c1e110b206115
anshu pasrija <anshu.pasrija@gmail.com>
2021-09-20 23:52:36 +0000

    seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,5 +2,11 @@
 INSERT INTO users (name, password, email, location, phone) VALUES
 ('Alice', 'labber', 'alice@gmail.com', 'Vancouver', 456789023),
  ('Kira', 'labber', 'kira@gmail.com', 'Vancouver', 234687650),
-('Eva Stanley','labber', 'sebastianguerra@ymail.com','vancouver',2345671);
+('Eva Stanley','labber', 'sebastianguerra@ymail.com','Surrey',2345671),
+('Dominic Parks', 'victoriablackwell@outlook.com','New-West',60787979),
+('Sue Luna','labber', 'jasonvincent@gmx.com','Richmond',4545454),
+('Rosalie Garza', 'labber','jacksondavid@gmx.com','Delta',64545464),
+('Etta West','labber', 'charlielevy@yahoo.com','Newton',778907654),
+('Margaret Wong','labber', 'makaylaweiss@icloud.com','Fleetwood',78765656677);
+
 
