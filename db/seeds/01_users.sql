55fb303c800ae84d7529559556312147540ff78d
anshu pasrija <anshu.pasrija@gmail.com>
2021-09-21 00:32:00 +0000

    added seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,12 +1,12 @@
--- Users table seeds here (Example)
-INSERT INTO users (name, password, email, location, phone) VALUES
-('Alice', 'labber', 'alice@gmail.com', 'Vancouver', 456789023),
- ('Kira', 'labber', 'kira@gmail.com', 'Vancouver', 234687650),
-('Eva Stanley','labber', 'sebastianguerra@ymail.com','Surrey',2345671),
-('Dominic Parks', 'victoriablackwell@outlook.com','New-West',60787979),
-('Sue Luna','labber', 'jasonvincent@gmx.com','Richmond',4545454),
-('Rosalie Garza', 'labber','jacksondavid@gmx.com','Delta',64545464),
-('Etta West','labber', 'charlielevy@yahoo.com','Newton',778907654),
-('Margaret Wong','labber', 'makaylaweiss@icloud.com','Fleetwood',78765656677);
+
+INSERT INTO users (name) VALUES
+('Alice'),
+ ('Kira'),
+('Eva Stanley'),
+('Dominic Parks'),
+('Sue Luna'),
+('Rosalie Garza'),
+('Etta West'),
+('Margaret Wong');
 
 
