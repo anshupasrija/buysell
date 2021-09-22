cf2bef2428656a42c3fb4b8bc7dad5aef8032292
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-21 23:01:27 -0700

    Add Sold mark on a sold trade

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,45 +22,29 @@ $(() => {
     }
   };
 
-  // <section class="shopping">
-  // <div class="shopping_subcontainer">
-  //   <h4 class="shopping_heading">Audi S7</h4>
-  //   <img src="../images/audiseven.png" class="shopping_image" alt="vehicle" class="vehicleimg"/>
-  // </div>
-  //   <div class="information">
-  //     <p>Year: 2019</p>
-  //     <p>Price: 75,000$</p>
-  //     <p>Status: ON SALE!</p>
-  //     <p>Color: White</p>
-  //     <p>Mileage: 14,000 km</p>
-  //     <p>Transmission: automatic</p>
-  //     <p>Fuel Type: Regular</p>
-  //     <button class="send-a-message">SEND A MESSAGE</button>
-  //   </div>
-  // </section>
-
 
   const createTradeElement = (trade) => {
 
-    const soldStr = (trade.sold === false) ? 'On Sale!' : 'Sold!'
+    const soldStr = (trade.sold === false) ? '' : '<h3>Sold</h3>'
+    const messageStr = (trade.sold === false) ? '<button class="send-a-message">SEND A MESSAGE</button>' : ''
 
     const $tradeElement = $(`
     <section class="shopping">
+    ${soldStr}
     <div class="shopping_subcontainer">
-      <h4 class="shopping_heading">Audi S7</h4>
+      <h4 class="shopping_heading">${trade.brand} ${trade.model}</h4>
       <img src="../images/audiseven.png" class="shopping_image" alt="vehicle" class="vehicleimg"/>
     </div>
       <div class="information">
         <p>Year: ${trade.year}</p>
         <p>Price: $${trade.price}</p>
-        <p>Status: ${soldStr}</p>
         <p>Color: ${trade.color}</p>
         <p>Mileage: ${trade.mileage}km</p>
         <p>Transmission: ${trade.transission}</p>
         <p>Fuel Type: ${trade.fuel}</p>
         <input id='trade-id' name='trade-id' type="text" value=${trade.id} hidden/>
         <a href="/messages?id=${trade.id}">
-        <button class="send-a-message">SEND A MESSAGE</button>
+        ${messageStr}
         </a>
       </div>
     </section>
