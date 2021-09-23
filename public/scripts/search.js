c8218e88db19e2d68f910a8234f179596c72921d
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-22 20:35:03 -0700

    Merge to lastest version

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,11 +1,92 @@
+$(() => {
+
+  console.log('loaded');
+
+  const loadTrades = () => {
+    // load the data
+    $.get('/api/trades')
+      .then((response) => {
+        renderTrades(response);
+      });
+
+    // $.ajax({
+    //   method: "GET",
+    //   url: "/api/trades"
+    // }).done((response) => {
+    //   // for(user of users) {
+    //   //   $("<div>").text(user.name).appendTo($("body"));
+    //   // }
+    //   console.log(response);
+    // });
+  };
+
+  const renderTrades = (trades) => {
+    console.log("trades->",typeof trades);
+    const $tradesContainer = $('#trade-container');
+    $tradesContainer.empty();
+
+    for (const trade of trades) {
+      const $trade = createTradeElement(trade);
+      console.log(trade);
+      $tradesContainer.append($trade);
+    }
+  };
+
+  const createTradeElement = (trade) => {
+
+    const soldStr = (trade.sold === false) ? '' : '<h3>Sold</h3>'
+    const soldCss = (trade.sold === false) ? '' : 'item_sold_parent'
+    const messageStr = (trade.sold === false) ? '<button type="button" class="btn btn-sm btn-outline-secondary">Message</button>' : ''
+
+    const $tradeElement = $(`
+      <div class="col-md-4">
+        <div id="item_sold_parent" class="card mb-4 box-shadow ${soldCss}" >
+        ${soldStr}
+          <img class="card-img-top" data-src="" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="../images/audiseven.png" data-holder-rendered="true">
+          <div class="card-body">
+            <p class="card-text">Brand: ${trade.brand}</p>
+            <p class="card-text">Model: ${trade.model}</p>
+            <p class="card-text">Year: ${trade.year}</p>
+            <p class="card-text">Price: $${trade.price}</p>
+            <p class="card-text">Color: ${trade.color}</p>
+            <p class="card-text">Mileage: ${trade.mileage}km</p>
+            <p class="card-text">Transmission: ${trade.transmission}</p>
+            <p class="card-text">Fuel Type: ${trade.fuel}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
+                <input id='trade-id' name='trade-id' type="text" value=${trade.id} hidden/>
+                ${messageStr}
+              </div>
+              <small class="text-muted">2 days ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
+    return $tradeElement;
+  };
 
 
-$(document).ready(()=>{
-
-  const loadCars= ()=>{
-  $.get('/search')
-  .then((response)=>{
-    console.log("i am response",response);
-
+  // grab the form
+  const $form = $('#search-trade-form');
+  $form.on('submit', (event) => {
+    event.preventDefault();
+    const data = $form.serialize();
+    console.log(data);
+    $.ajax({
+      method: "GET",
+      url: `/search?${data}`,
+    })
+    .then(function(data) {
+      renderTrades(data);
+      $(".error").hide(250);
+      $form.trigger("reset"); // reset the form like refreshing
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   });
-};
+
+  loadTrades();
+});
--- routes/favourites.js
+++ routes/favourites.js
@@ -26,24 +26,4 @@ module.exports = (db) => {
 };
 
 
-module.exports = (db) => {
 
-  router.get("/:user_id", (req, res) => {
-    console.log('we are here');
-    const sqlQuery = SELECT * FROM favourites JOIN trades ON  trades.id =favourites.trade_id WHERE favourites.user_id =$1;
-    const params = [req.cookies.user_id]
-    console.log("this is params", params);
-    db.query(sqlQuery,params)
-    .then(data => {
-      console.log('<<<<<<<<<<<<<<<<<>>>>>>>>>>', data.rows)
-      return res.status(200).json(data.rows);
-    })
-  });
-
-  // router.post("/", (req,res)=>{
-  //   console.log(req.body);
-  // })
-
-
-  return router;
-};
