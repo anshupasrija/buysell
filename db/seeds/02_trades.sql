6eeb6b94acef8b08c4be155e2c3a8b1ba8e09e98
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-23 19:59:19 -0700

    Modify trades table seed file

--- db/seeds/02_trades.sql
+++ db/seeds/02_trades.sql
@@ -7,19 +7,27 @@ INSERT INTO trades
     transmission,
     fuel,
     mileage,
-    thumbnail_photo_url,
     image,
     price,
     description,
     sold
   ) VALUES
-    ( 'KIA', 'Sedona', '2021', 'Black', 'Auto', 'Gasoline', 1000, '', '', 30000, 'description',  false),
-    ( 'Honda', 'Model', '2020', 'Black', 'Auto', 'Gasoline', 2000, '', '', 40000, 'description',  true),
-    ( 'BMW', 'Model', '2019', 'Black', 'Auto', 'Gasoline', 3000, '', '', 50000, 'description',  false),
-    ( 'NISAN', 'Model', '2018', 'Black', 'Auto', 'Gasoline', 4000, '', '', 60000, 'description',  false),
-    ( 'TOYOTA', 'Model', '2017', 'Black', 'Auto', 'Gasoline', 5000, '', '', 70000, 'description',  false),
-    ( 'BRAND1', 'Model1', '2016', 'Black', 'Auto', 'Gasoline', 6000, '', '', 80000, 'description',  false),
-    ( 'BRAND2', 'Model2', '2015', 'Black', 'Auto', 'Gasoline', 7000, '', '', 90000, 'description',  false),
-    ( 'BRAND3', 'Model3', '2014', 'Black', 'Auto', 'Gasoline', 8000, '', '', 95000, 'description',  false),
-    ( 'BRADN4', 'Model4', '2013', 'Black', 'Auto', 'Gasoline', 9000, '', '', 35000, 'description',  false)
-;
+    ( 'KIA', 'Sedona', '2021', 'black', 'automatic', 'regular', 5000,  'audiseven.png', 450000, 'description',  false),
+    ( 'Audi', 'A4', '2020', 'silver', 'automatic', 'regular', 15000,  'audiseven.png', 50000, 'description',  true),
+    ( 'BMW', '318', '2014', 'black', 'automatic', 'regular', 200000,  'Screen Shot 2021-09-22 at 11.37.17 AM.png', 30000, 'description',  false),
+    ( 'Audi', 'A4', '2018', 'black', 'manual', 'regular', 20000,  'audiseven.png', 50000, 'description',  false),
+    ( 'BMW', '540', '2019', 'black', 'Auto', 'regular', 30000,  'audiseven.png', 50000, 'description',  false),
+    ( 'Mercedes', 'C450 AMG', '2018', 'red', 'Auautomaticto', 'regular', 6000,  'bmw.webp', 60000, 'description',  false),
+    ( 'Mercedes', 'C450 AMG', '2017', 'black', 'automatic', 'regular', 70000,  'Screen Shot 2021-09-22 at 12.12.33 PM.png', 70000, 'description',  false),
+    ( 'BMW', '318', '2016', 'black', 'automatic', 'regular', 100000,  'bmw.webp', 15000, 'description',  false),
+    ( 'BMW', '318', '2015', 'black', 'automatic', 'regular', 100000,  'bmw.webp', 17000, 'description',  false),
+    ( 'BMW', '318', '2014', 'black', 'automatic', 'regular', 100000,  'bmw.webp', 20000, 'description',  false),
+    ( 'BMW', '318', '2018', 'black', 'automatic', 'regular', 90000,  'bmw.webp', 35000, 'description',  true),
+    ( 'BMW', '318', '2019', 'white', 'automatic', 'regular', 90000,  'bmw.webp', 35000, 'description',  false),
+    ( 'BMW', '540', '2006', 'black', 'automatic', 'regular', 70000,  'bmw.webp', 35000, 'description',  false),
+    ( 'Audi', 'A4', '2007', 'black', 'manual', 'regular', 60000,  'audiseven.png', 25000, 'description',  false),
+    ( 'Audi', 'A4', '2020', 'black', 'automatic', 'regular', 40000,  'audiseven.png', 30000, 'description',  false),
+    ( 'Mercedes', 'C450 AMG', '2007', 'black', 'manual', 'regular', 9000,  'Screen Shot 2021-09-22 at 12.12.33 PM.png', 50000, 'description',  false),
+    ( 'BMW', '540', '2014', 'black', 'automatic', 'regular', 100000,  'Screen Shot 2021-09-22 at 11.37.17 AM.png', 40000, 'description',  true),
+    ( 'Mercedes', 'C450 AMG', '2019', 'black', 'automatic', 'regular', 70000,  'Screen Shot 2021-09-22 at 11.37.17 AM.png', 50000, 'description',  false)
+    ;
